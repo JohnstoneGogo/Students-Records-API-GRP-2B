d08b78a939c98a9eeb5ff1f9a339e83e3df67a93
Opeyemi Ogunshina <ogunshinaopeyemi07@gmail.com>
2026-03-23 23:52:03 +0100

    Opeyemi added the post request and a get route for retrieving the database

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+const crypto = require('crypto');
+
+//in-memory data store for students
+const studentCollection = [];
 
 app.use(express.json());
 
 // Sample route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/students', (req, res) => {
+  res.json(studentCollection);
+});
+
+//FOR CREATING A SINGLE USER
+app.post("/students", (req, res) => {
+  const { name, email, matricNumber, courses } = req.body;
+
+  if (!name || !email || !matricNumber || !courses) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const newStudent = { id: crypto.randomUUID(), name, email, matricNumber, courses };
+  studentCollection.push(newStudent);
+
+  res.status(201).json({ message: "New student added successfully", Student: newStudent });
+  console.log(`Message: new student alert`);
+  console.log("Student record created:", newStudent);
 });
 
 app.listen(port, () => {
